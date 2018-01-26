2f14446e24d39ba86700d62e8622641ef0b66e07
Bogdan Manshilin <skimphiter@gmail.com>
2018-01-26 21:18:48 +0200

    Initial Commit

---
+++ BigInc/BigInc.h
@@ -0,0 +1,19 @@
+//
+//  BigInc.h
+//  BigInc
+//
+//  Created by Богдан Маншилин on 26/01/2018.
+//  Copyright © 2018 BManshilin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for BigInc.
+FOUNDATION_EXPORT double BigIncVersionNumber;
+
+//! Project version string for BigInc.
+FOUNDATION_EXPORT const unsigned char BigIncVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BigInc/PublicHeader.h>
+
+
